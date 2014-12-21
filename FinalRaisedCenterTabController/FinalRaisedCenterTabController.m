43668f8f3fd33227d05a91bbff5377e4fcc19e06
Derek Johnson <derekj212@gmail.com>
2014-12-21 13:53:31 -0800

    Change from center button to floating action button style

--- FinalRaisedCenterTabController/FinalRaisedCenterTabController.m
+++ FinalRaisedCenterTabController/FinalRaisedCenterTabController.m
@@ -28,14 +28,15 @@
         
         UIImage *image = [self.centerButton imageForState:UIControlStateNormal];
         
-        self.centerButton.frame = CGRectMake(0.0, 0.0, image.size.width, image.size.height);
+        self.centerButton.frame = CGRectMake(0.0, 0.0, 70, 70);
         
-        CGFloat heightDifference = image.size.height - self.tabBar.frame.size.height;
-        
-        CGPoint center = self.tabBar.center;
-        if (heightDifference > 0) {
-            center.y = center.y - heightDifference/2.0;
-        }
+//        CGFloat heightDifference = image.size.height - self.tabBar.frame.size.height;
+//        
+         CGPoint center = self.tabBar.center;
+//        if (heightDifference > 0) {
+            center.y = self.view.bounds.size.height - (self.centerButton.frame.size.height / 2) - 10;
+            center.x = self.view.bounds.size.width - (self.centerButton.frame.size.width / 2) - 10;
+//        }
         self.centerButton.center = center;
         [self.view bringSubviewToFront:self.centerButton];
     }
